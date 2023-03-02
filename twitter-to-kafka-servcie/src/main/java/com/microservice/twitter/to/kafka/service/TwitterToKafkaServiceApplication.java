0bcc85cf6d4de18dfa288e606485bc53bd3177a3
ngel rojas pacheco <angel.pacheco@ntendencia.com.br>
2023-03-02 15:47:58 -0500

    add: adding new classes kafka interfaces

--- twitter-to-kafka-servcie/src/main/java/com/microservice/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-servcie/src/main/java/com/microservice/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.microservice")
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
--- twitter-to-kafka-servcie/src/main/java/com/microservice/twitter/to/kafka/service/runner/impl/TwitterKafkaStreamRunner.java
+++ twitter-to-kafka-servcie/src/main/java/com/microservice/twitter/to/kafka/service/runner/impl/TwitterKafkaStreamRunner.java
@@ -6,6 +6,7 @@ import jakarta.annotation.PreDestroy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.microservice.twitter.to.kafka.service.runner.StreamRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import twitter4j.FilterQuery;
 import twitter4j.TwitterException;
@@ -15,6 +16,7 @@ import twitter4j.TwitterStreamFactory;
 import java.util.Arrays;
 
 @Component
+@ConditionalOnExpression("${twitter-to-kafka-service.enable-mock-tweets} && not ${twitter-to-kafka-service.enable-v2-tweets}")
 public class TwitterKafkaStreamRunner implements StreamRunner {
         private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaStreamRunner.class);
         private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
